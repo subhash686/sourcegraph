9da5109a098d4c7f6177197e3df2445cd79baae7
Eric Fritz <eric@eric-fritz.com>
2022-04-20 18:06:05 -0500

    Add transport.

---
+++ internal/codeintel/policies/transport/graphql/resolver.go
@@ -0,0 +1,29 @@
+package graphql
+
+import (
+	"context"
+
+	policies "github.com/sourcegraph/sourcegraph/internal/codeintel/policies"
+	"github.com/sourcegraph/sourcegraph/internal/observation"
+)
+
+type Resolver struct {
+	svc        *policies.Service
+	operations *operations
+}
+
+func newResolver(svc *policies.Service, observationContext *observation.Context) *Resolver {
+	return &Resolver{
+		svc:        svc,
+		operations: newOperations(observationContext),
+	}
+}
+
+func (r *Resolver) Todo(ctx context.Context) (err error) {
+	ctx, endObservation := r.operations.todo.With(ctx, &err, observation.Args{})
+	defer endObservation(1, observation.Args{})
+
+	// TODO
+	_, err = r.svc.List(ctx, policies.ListOpts{})
+	return err
+}
