17ccc587458938c1b2de1bd802d3d5f93b8cd315
Eric Fritz <eric@eric-fritz.com>
2022-04-20 18:04:01 -0500

    Add transport.

---
+++ internal/codeintel/symbols/transport/graphql/resolver.go
@@ -0,0 +1,29 @@
+package graphql
+
+import (
+	"context"
+
+	symbols "github.com/sourcegraph/sourcegraph/internal/codeintel/symbols"
+	"github.com/sourcegraph/sourcegraph/internal/observation"
+)
+
+type Resolver struct {
+	svc        *symbols.Service
+	operations *operations
+}
+
+func newResolver(svc *symbols.Service, observationContext *observation.Context) *Resolver {
+	return &Resolver{
+		svc:        svc,
+		operations: newOperations(observationContext),
+	}
+}
+
+func (r *Resolver) Todo(ctx context.Context) (err error) {
+	ctx, endObservation := r.operations.todo.With(ctx, &err, observation.Args{})
+	defer endObservation(1, observation.Args{})
+
+	// TODO
+	_, err = r.svc.Symbol(ctx, symbols.SymbolOpts{})
+	return err
+}
